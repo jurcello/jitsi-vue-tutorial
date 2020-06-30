f24dc3d12e067bac18ca0e4a8dc753e51ac85af1
Jur de Vries <info@jurdevries.nl>
2020-06-30 15:24:06 +0200

    Step 2: Successful connection.

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,32 @@
 </template>
 
 <script>
+import JitsiMeetJS from '@lyno/lib-jitsi-meet';
+import $ from 'jquery';
+window.$ = $;
+
+import options from './options/config';
+
+JitsiMeetJS.init();
+JitsiMeetJS.setLogLevel(JitsiMeetJS.logLevels.INFO);
+
+const connection = new JitsiMeetJS.JitsiConnection(null, null, options);
+
+function onConnectionSuccess() {
+  console.log("onConnectionSuccess", arguments);
+}
+function onConnectionFailed() {
+  console.log("onConnectionFailed", arguments);
+}
+function disconnect() {
+  console.log("disconnect", arguments);
+}
+
+connection.addEventListener(JitsiMeetJS.events.connection.CONNECTION_ESTABLISHED, onConnectionSuccess);
+connection.addEventListener(JitsiMeetJS.events.connection.CONNECTION_FAILED, onConnectionFailed);
+connection.addEventListener(JitsiMeetJS.events.connection.CONNECTION_DISCONNECTED, disconnect);
+
+connection.connect();
 
 export default {
   name: 'App',
