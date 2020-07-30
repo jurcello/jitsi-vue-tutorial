5f8fdd64b5c4271b94da10d980edda4a9cc26c71
Jur de Vries <info@jurdevries.nl>
2020-07-30 16:05:53 +0200

    Step 3: Successful room join and creation of local tracks.

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,9 @@
 <script>
 import JitsiMeetJS from '@lyno/lib-jitsi-meet';
 import $ from 'jquery';
-window.$ = $;
-
 import options from './options/config';
+
+window.$ = $;
 
 JitsiMeetJS.init();
 JitsiMeetJS.setLogLevel(JitsiMeetJS.logLevels.INFO);
@@ -18,10 +18,29 @@ const connection = new JitsiMeetJS.JitsiConnection(null, null, options);
 
 function onConnectionSuccess() {
   console.log("onConnectionSuccess", arguments);
+  const room = connection.initJitsiConference("my-secret-conference", {});
+  room.on(JitsiMeetJS.events.conference.TRACK_ADDED, onRemoteTrack);
+  room.on(JitsiMeetJS.events.conference.CONFERENCE_JOINED, () => {
+    console.log("onConferenceJoined", arguments);
+    JitsiMeetJS.createLocalTracks({
+      devices: ['video', 'audio']
+    }).then((tracks) => {
+      console.log("onLocalTracks", tracks);
+      tracks.forEach(track => {
+        room.addTrack(track);
+      })
+    }).catch(error => {
+          console.error("There was an error creating the local tracks:", error);
+        }
+    );
+  });
+  room.join();
 }
+
 function onConnectionFailed() {
   console.log("onConnectionFailed", arguments);
 }
+
 function disconnect() {
   console.log("disconnect", arguments);
 }
@@ -31,6 +50,12 @@ connection.addEventListener(JitsiMeetJS.events.connection.CONNECTION_FAILED, onC
 connection.addEventListener(JitsiMeetJS.events.connection.CONNECTION_DISCONNECTED, disconnect);
 
 connection.connect();
+
+
+function onRemoteTrack() {
+  console.log("onRemoteTrack", arguments);
+}
+
 
 export default {
   name: 'App',
